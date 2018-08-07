43af8ef666b153c47a53b4994299a2b69f9edd3c
Berchem <34000467+Berchem@users.noreply.github.com>
2018-08-07 23:35:34 +0800

    v0.1.2

--- 0_basic/basic_bar.js
+++ 0_basic/basic_bar.js
@@ -15,3 +15,25 @@ function bar01(id){
 
     Plotly.newPlot(id, data);
 }
+
+function bar02(id){
+    var trace = []
+    trace[0] = {
+      x: ['giraffes', 'orangutans', 'monkeys'],
+      y: [20, 14, 23],
+      name: 'SF Zoo',
+      type: 'bar'
+    };
+
+    trace[1] = {
+      x: ['giraffes', 'orangutans', 'monkeys'],
+      y: [12, 18, 29],
+      name: 'LA Zoo',
+      type: 'bar'
+    };
+
+
+    var layout = {barmode: 'group'};
+
+    Plotly.newPlot(id, trace, layout);
+}
